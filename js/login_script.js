f1a68b131298929051527aec1c97c18975528116
vivek997 <vivekrawal997@gmail.com>
2018-04-16 23:16:43 +0530

    login check

--- js/login_script.js
+++ js/login_script.js
@@ -1,22 +1,48 @@
+
+var provider = new firebase.auth.GoogleAuthProvider();
+var user;
+
 $( document ).ready(function() {
 	$("#welcome").hide();
 });
 
-
-
 function login(){
-    function newLoginHappened(user){
-        if (user){
-            //user is signed in
-            showWelcomeContainer();
-        }
-        else{
-            var provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithRedirect(provider);
-        }
-    }
+//    function newLoginHappened(user){
+//        if (user){
+//            //user is signed in
+//            showWelcomeContainer();
+//        }
+//        else{
+//            var provider = new firebase.auth.GoogleAuthProvider();
+//            firebase.auth().signInWithRedirect(provider);
+//        }
+//    }
+//    
+//    firebase.auth().onAuthStateChanged(newLoginHappened);
     
-    firebase.auth().onAuthStateChanged(newLoginHappened);
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  user = result.user;
+	  showWelcomeContainer();
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+    
+    
+    
+    
+    
+    
 };
 
 function showWelcomeContainer() {
