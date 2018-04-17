310e18b5767b78e1df7758de6016705ecf81ffa9
vivek997 <vivekrawal997@gmail.com>
2018-04-17 14:16:37 +0530

    login check

--- js/login_script.js
+++ js/login_script.js
@@ -1,36 +1,28 @@
-var provider = new firebase.auth.GoogleAuthProvider();
-var user;
-
 
 $( document ).ready(function() {
-	$("#welcome").hide();
+	$("#user_div").hide();
 });
 
 function signIn() {
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a Google Access Token. You can use it to access the Google API.
-	  var token = result.user.uid;
-	  // The signed-in user info.
-	  user = result.user;
-	  showWelcomeContainer();
-	  sessionStorage.token = token;
-	  // ...
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  // ...
-	});
+     function newLoginHappened(user){
+        if (user){
+            //user is signed in
+            showWelcomeContainer();
+        }
+        else{
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider);
+        }
+    }
+    
+    firebase.auth().onAuthStateChanged(newLoginHappened);
+	
 
 };
 
 function showWelcomeContainer() {
-	$("#login").hide();
-	$("#welcome").show();
+	$("#login_div").hide();
+	$("#user_div").show();
 	$("#welcomeText").html("Hello, " + user.displayName);
 };
 
@@ -41,17 +33,24 @@ firebase.auth().signOut();
 
 
 
-//    function newLoginHappened(user){
-//        if (user){
-//            //user is signed in
-//            showWelcomeContainer();
-//        }
-//        else{
-//            var provider = new firebase.auth.GoogleAuthProvider();
-//            firebase.auth().signInWithRedirect(provider);
-//        }
-//    }
-//    
-//    firebase.auth().onAuthStateChanged(newLoginHappened);
+   
 
 
+//firebase.auth().signInWithPopup(provider).then(function(result) {
+//	  // This gives you a Google Access Token. You can use it to access the Google API.
+//	  var token = result.user.uid;
+//	  // The signed-in user info.
+//	  user = result.user;
+//	  showWelcomeContainer();
+//	  sessionStorage.token = token;
+//	  // ...
+//	}).catch(function(error) {
+//	  // Handle Errors here.
+//	  var errorCode = error.code;
+//	  var errorMessage = error.message;
+//	  // The email of the user's account used.
+//	  var email = error.email;
+//	  // The firebase.auth.AuthCredential type that was used.
+//	  var credential = error.credential;
+//	  // ...
+//	});
