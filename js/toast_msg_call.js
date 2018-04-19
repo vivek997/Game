e106eaf085554a5e959273d9974c0aa946906233
vivek997 <vivekrawal997@gmail.com>
2018-04-19 19:04:59 +0530

    toast

---
+++ js/toast_msg_call.js
@@ -0,0 +1,60 @@
+
+    function showSuccessToast() {
+        $().toastmessage('showSuccessToast', "Successfully Login");
+    }
+    function showStickySuccessToast() {
+        $().toastmessage('showToast', {
+            text     : 'Success Dialog which is sticky',
+            sticky   : true,
+            position : 'top-right',
+            type     : 'success',
+            closeText: '',
+            close    : function () {
+                console.log("toast is closed ...");
+            }
+        });
+
+    }
+    function showNoticeToast() {
+        $().toastmessage('showNoticeToast', "Notice  Dialog which is fading away ...");
+    }
+    function showStickyNoticeToast() {
+        $().toastmessage('showToast', {
+             text     : 'Notice Dialog which is sticky',
+             sticky   : true,
+             position : 'top-right',
+             type     : 'notice',
+             closeText: '',
+             close    : function () {console.log("toast is closed ...");}
+        });
+    }
+    function showWarningToast() {
+        $().toastmessage('showWarningToast', "Warning Dialog which is fading away ...");
+    }
+    function showStickyWarningToast() {
+        $().toastmessage('showToast', {
+            text     : 'Warning Dialog which is sticky',
+            sticky   : true,
+            position : 'top-right',
+            type     : 'warning',
+            closeText: '',
+            close    : function () {
+                console.log("toast is closed ...");
+            }
+        });
+    }
+    function showErrorToast() {
+        $().toastmessage('showErrorToast', "Error Dialog which is fading away ...");
+    }
+    function showStickyErrorToast() {
+        $().toastmessage('showToast', {
+            text     : 'Error Dialog which is sticky',
+            sticky   : true,
+            position : 'top-right',
+            type     : 'error',
+            closeText: '',
+            close    : function () {
+                console.log("toast is closed ...");
+            }
+        });
+    }
